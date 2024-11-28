fd33e038e4c6e8ec752cd3aacab6856dce0e45eb
frang6 <garciapedrajas21@gmail.com>
2024-11-28 15:27:51 +0100

    terminado, pueden hacerse mejoras

--- UD04_Retos/Ejercicio5/u4e5_modificarelementos.js
+++ UD04_Retos/Ejercicio5/u4e5_modificarelementos.js
@@ -1,3 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Seleccionamos todos los botones
+    const botones = document.querySelectorAll(".botones button");
+    
+    //le añadimos a cada boton su evento de click
+    botones.forEach(boton => {
+        boton.addEventListener("click", () => {
+            // Saca el id del botón seleccionado que es el equipo
+            const equipoSeleccionado = boton.id;
+
+            // Quitamos formato a todas las imágenes 
+            const imagenes = document.querySelectorAll(".imagenes img");
+            imagenes.forEach(img => {
+                img.style.border = "none"; 
+                img.style.transform = "scale(1)"; 
+            });
+
+            // Resalta las imágenes con el id que coincide con el equipo seleccionado
+            const imagenesEquipo = document.querySelectorAll(".imagenes img");
+            imagenesEquipo.forEach(img => {
+                if(img.id === equipoSeleccionado) {         //Si coincide con el id del equipo seleccionado, quiere decir que pertenece a ese equipo, por lo tanto se resalta
+                    img.style.border = "5px solid limegreen"; // Aplica el borde verde llamativo
+                    img.style.transform = "scale(1.1)"; // Aumenta el tamaño de la imagen
+                }
+            });
+        });
+    });
+});
+
 const jugadoresInfo = {
     Mbappe: "Kylian Mbappé, delantero estrella del Real Madrid y de la selección francesa, conocido por su velocidad y habilidad goleadora.",
     Haaland: "Erling Haaland, delantero del Manchester City, considerado uno de los mejores goleadores jóvenes del mundo por su potencia y precisión.",
@@ -13,3 +42,4 @@ const mostrarInformacion = (jugador) => {
     const informacion = document.getElementById('panelDatos');
     informacion.innerHTML = `<h2>Información de Jugador</h2><p>${jugadoresInfo[jugador]}</p>`;
 };
+
